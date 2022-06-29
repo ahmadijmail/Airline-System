b0406cb850315ff58c75d7c2aa19b234d704097b
ahmadijmail <ahmadijmil2010@gmail.com>
2022-06-29 15:16:52 +0300

    messageQueue

--- pilot/pilot.js
+++ pilot/pilot.js
@@ -5,22 +5,24 @@ const io = require("socket.io-client");
 let host = `http://localhost:${process.env.PORT}/airline`;
 const airlineconn = io.connect(host);
 
+let host2 = `http://localhost:${process.env.PORT}/`;
+const airlineconn2 = io.connect(host2);
+
+
 airlineconn.on("new-flight", newFlight);
+airlineconn2.emit("get_all");
+airlineconn2.on("flight", handelflight);
 
-// const airlineconn = io.connect(host);
-// airlineconn.on("new-flight", tookof);
-
-// function tookof(payload) {
-//   setTimeout(() => {
-//     console.log(`Pilot: flight with ID '${payload.Details.flightID}' took-off`);
-//     sysConnection.emit("took-off", payload);
-//   }, 4000);
-// }
+function handelflight(flight){
+  console.log(`Pilot:Sorry i didn't catch this flight ID ${flight.id} `);
+  airlineconn2.emit("deleteQue",flight.id );
+}
 
 function newFlight(payload) {
   setTimeout(() => {
     console.log(`Pilot: flight with ID '${payload.Details.flightID}' took-off`);
     airlineconn.emit("took-off", payload);
+    
   }, 4000);
 
   setTimeout(() => {
--- system/system.js
+++ system/system.js
@@ -4,30 +4,44 @@ const PORT = process.env.PORT || 3000;
 
 const ioServer = require("socket.io")(PORT);
 const airline = ioServer.of("/airline");
+const uuid = require("uuid").v4;
 
-
-
+let queue = {
+  flights: {},
+};
 airline.on("connection", (socket) => {
-    console.log("Connected to Airline ", (socket.id));
-        socket.on("took-off", tookOff);
-        function tookOff(payload) {
-          console.log(payload.flight);
-        }
-    
-      socket.on("arrived", arrivedhandel);
-      function arrivedhandel(payload) {
-        console.log(payload.flight);
-      }
-    });
-
-ioServer.on("connection", (socket) => {
-console.log("Connected to Manager");
-
-  socket.on("new-flight", newflight);
-  function newflight(payload) {
+  console.log("Connected to Airline ", socket.id);
+  socket.on("took-off", tookOff);
+  function tookOff(payload) {
     console.log(payload.flight);
-    airline.emit("new-flight", payload)
   }
 
- 
+  socket.on("arrived", arrivedhandel);
+  function arrivedhandel(payload) {
+    console.log(payload.flight);
+  }
+});
+
+ioServer.on("connection", (socket) => {
+  socket.on("new-flight", newflight);
+  function newflight(payload) {
+    const id = uuid();
+    queue.flights[id] = payload;
+    console.log(payload.flight);
+    airline.emit("new-flight", payload);
+  }
+  socket.on("get_all", () => {
+    
+    Object.keys(queue.flights).forEach((id) => {
+      socket.emit("flight", {
+        id: id,
+        payload: queue.flights[id],
+      });
+    });
+  });
+
+  socket.on("deleteQue", (queue) => {
+   delete queue.flights.id;
+    console.log("unread flights done and deleted");
+  });
 });
